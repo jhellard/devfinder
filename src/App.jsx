877a6020b667cbc2ce3d2a52fd20746be9766241
Joshua Hellard <67675776+jhellard@users.noreply.github.com>
2023-02-21 14:13:33 -0500

    Fixed date format, changed some margins

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ const App = () => {
       <Header />
       <main className="max-w-[621px] md:max-w-[778px] mx-auto">
         <SearchBar inputRef={userInput} isError={isError} />
-        <section className="mx-6 bg-veryLightWhite rounded-2xl pt-8 mb-8 sm:mb-0 px-6 md:px-12 shadow-lg dark:bg-darkModeBlue md:flex gap-[37px]">
+        <section className="mx-6 bg-veryLightWhite rounded-2xl pt-8 mb-8 sm:mb-0 px-6 sm:px-10 md:px-12 shadow-lg dark:bg-darkModeBlue md:flex gap-[37px]">
           <img
             className="max-w-[117px] max-h-[117px] rounded-full hidden md:block"
             src={data.avatar_url}
@@ -64,14 +64,14 @@ const App = () => {
                   </span>
                 )}
                 <h2 className="text-[13px] text-grayishBlue dark:text-white sm:text-[15px] md:col-start-2 md:row-start-1 justify-self-end self-center">
-                  Joined {moment(data.created_at).format("D MMM YY")}
+                  Joined {moment(data.created_at).format("D MMM YYYY")}
                 </h2>
               </div>
             </div>
             <p className="text-[13px] text-lightBlue mb-6 dark:text-white sm:text-[15px] sm:mb-8">
               {data.bio ?? "This profile has no bio"}
             </p>
-            <ul className="px-4 sm:px-8 bg-lightBg dark:bg-darkBg min-h-[85px] mb-6 flex items-center rounded-[10px] justify-between sm:justify-start text-darkBg dark:text-white">
+            <ul className="px-4 sm:px-8 bg-lightBg dark:bg-darkBg min-h-[85px] mb-6 sm:mb-[30px] md:mb-[37px] flex items-center rounded-[10px] justify-between sm:justify-start text-darkBg dark:text-white">
               <Stats name="Repos" data={data.public_repos} spacing={99} />
               <Stats name="Followers" data={data.followers} spacing={82} />
               <Stats name="Following" data={data.following} />
